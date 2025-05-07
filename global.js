f88c074a1a897c4a3d8f623327f898db06b7dafc
ddho22 <158776005+ddho22@users.noreply.github.com>
2025-05-07 14:45:53 -0700

    fsadfasdfasd

--- global.js
+++ global.js
@@ -17,6 +17,7 @@ let pages = [
     { url: 'projects/', title: 'Projects' },
     { url: 'contact/', title: 'Contact' },
     { url: 'Resume/', title: 'Resume' },
+    { url: 'meta/', title: 'Meta'},
     { url: 'https://github.com/ddho22', title: 'Github' },
   ];
 
